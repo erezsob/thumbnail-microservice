a835a0e800cab6bdff51cfdd89e051d37eaca296
Erez Sobel <sobelerez@gmail.com>
2017-05-08 23:17:29 +0200

    Set up a server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+const api = require('./routes/api');
+
+app.use('/', api);
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Running app on PORT ${port}`);
+  }
+});
