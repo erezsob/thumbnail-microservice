0c0e6cda33292ca44a936b170046aed751b87f2a
Erez Sobel <sobelerez@gmail.com>
2017-05-08 23:27:09 +0200

    Set up unit testing

---
+++ thumbnail_service.test.js
@@ -0,0 +1,9 @@
+
+const { expect } = require('chai');
+const thumbnail_service = require('./thumbnail_service');
+
+describe('First test', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
