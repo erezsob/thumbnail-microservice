6362b6091b4d0239d5b46f5840f310f8baeeba81
Erez Sobel <sobelerez@gmail.com>
2017-05-08 23:28:46 +0200

    Set up the thumbnail service functions

---
+++ thumbnail_service.js
@@ -0,0 +1,11 @@
+const varifyUrlBase64 = () => {};
+const varifyMaxWidth = () => {};
+const varifyMaxHeight = () => {};
+const varifySignatureBase64 = () => {};
+const varifyExtension = () => {};
+
+module.exports.varifyUrlBase64 = varifyUrlBase64;
+module.exports.varifyMaxWidth = varifyMaxWidth;
+module.exports.varifyMaxHeight = varifyMaxHeight;
+module.exports.varifySignatureBase64 = varifySignatureBase64;
+module.exports.varifyExtension = varifyExtension;
