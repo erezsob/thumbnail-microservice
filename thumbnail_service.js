8770dc4b6e3c96b6e699e6c3065729dfb8db4af6
Erez Sobel <sobelerez@gmail.com>
2017-05-10 12:36:57 +0200

    verify the maxHeight and maxWidth values

--- thumbnail_service.js
+++ thumbnail_service.js
@@ -11,10 +11,15 @@ const verifyUrlBase64 = x => pipe(
 );
 
 const checkUrlValidity = x => validUrl.isUri(x) ? x : false;
+
 const decode = x => Buffer.from(x, 'base64').toString('ascii');
 
+const verifyMaxWidthHeight = (x, prop) => {
+  const newInt = x[prop] !== 'Number' ? parseInt(x[prop]) : x[prop];
+  return !isNaN(newInt) && +3 < newInt && newInt < +1024 ? newInt : false;
+}
 
-const verifyMaxHeight = x => {x.maxHeight}
+
 const verifySignatureBase64 = x => {x.signatureBase64}
 const verifyExtension = x => {x.extension}
 
@@ -22,3 +27,4 @@ const verifyExtension = x => {x.extension}
 module.exports = thumbnailService;
 module.exports.checkUrlValidity = checkUrlValidity;
 module.exports.decode = decode;
+module.exports.verifyMaxWidthHeight = verifyMaxWidthHeight;
