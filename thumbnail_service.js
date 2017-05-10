0e40e4b019e729a823ff76764f4fb3b28e706575
Erez Sobel <sobelerez@gmail.com>
2017-05-10 17:03:03 +0200

    Check that extention if one of the allowed ones

--- thumbnail_service.js
+++ thumbnail_service.js
@@ -3,17 +3,34 @@
 const { pipe } = require('lodash/fp');
 const validUrl = require('valid-url');
 
+const thumbnailService = (req, res) => {
+  return validity(req);
+}
 
-// urlBase64
+/**
+ * Composing the function
+ */
 const verifyUrlBase64 = x => pipe(
   decode,
   checkUrlValidity
 );
 
-const checkUrlValidity = x => validUrl.isUri(x) ? x : false;
+/**
+ * Check Url Validity
+ * Returns true or false
+ */
+const checkUrlValidity = x => validUrl.isUri(x) ? true : false;
 
+/**
+ * Decoding data from Base64 to regular string (URL string if it's valid)
+ * Return the decoded string
+ */
 const decode = x => Buffer.from(x, 'base64').toString('ascii');
 
+/**
+ * Verifying that the maxWidth and maxHeight are according to the rules
+ * Returns transformed data or false if failed
+ */
 const verifyMaxWidthHeight = (x, prop) => {
   const newInt = x[prop] !== 'Number' ? parseInt(x[prop]) : x[prop];
   return !isNaN(newInt) && +3 < newInt && newInt < +1024 ? newInt : false;
@@ -21,10 +38,16 @@ const verifyMaxWidthHeight = (x, prop) => {
 
 
 const verifySignatureBase64 = x => {x.signatureBase64}
-const verifyExtension = x => {x.extension}
+
+/**
+ * Check that the property is one of the desired extensions options
+ * Returns true or false
+ */
+const verifyExtension = x => /^jpeg|png|gif|ico|webm$/.test(x);
 
 
 module.exports = thumbnailService;
 module.exports.checkUrlValidity = checkUrlValidity;
 module.exports.decode = decode;
 module.exports.verifyMaxWidthHeight = verifyMaxWidthHeight;
+module.exports.verifyExtension = verifyExtension;
