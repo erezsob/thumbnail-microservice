ab5579e0e3fdbcca48e1af0fba70b5994952b5e8
Erez Sobel <sobelerez@gmail.com>
2017-05-10 11:09:55 +0200

    Add test for checkUrlValidity

--- thumbnail_service.test.js
+++ thumbnail_service.test.js
@@ -1,9 +1,35 @@
 
 const { expect } = require('chai');
-const thumbnail_service = require('./thumbnail_service');
+const thumbnailService = require('./thumbnail_service');
+const validUrl = require('valid-url');
 
+
+/* eslint-disable no-undef */
 describe('First test', () => {
   it('should pass', () => {
-    expect(true).to.equal(true);
-  });
-});
+    expect(true).to.equal(true)
+  })
+})
+
+describe('thumbnailService', () => {
+  describe.only('thumbnailService.checkUrlValidity', () => {
+    it('should check if a string is a url', (done) => {
+      const testData = [
+        {
+          inputData: 'http://www.example.com',
+          expectedData: 'http://www.example.com'
+        },
+        {
+          inputData: 'abcdefg',
+          expectedData: false
+        }
+      ]
+
+      testData.forEach((testItem) => {
+        const returnData = thumbnailService.checkUrlValidity(testItem.inputData);
+        expect(returnData).to.equal(testItem.expectedData);
+      });     
+    })
+  })
+
+})
