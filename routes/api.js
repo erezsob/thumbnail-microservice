efba40c05e485a642dc5e3b063343bd998d9dfec
Erez Sobel <sobelerez@gmail.com>
2017-05-11 13:06:45 +0200

    Handle exceptions with the api call

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,13 @@ const router = express.Router()
 
 const thumbnailService = require('../thumbnail_service')
 
-// router.route('/:urlBase64/:maxWidth/:maxHeight/:signatureBase64.:extension').get(thumbnailService);
-router.route('/').get(thumbnailService)
+router.route('/:urlBase64/:maxWidth/:maxHeight/:signatureBase64.:extension')
+.get(thumbnailService)
+.catch(error => {
+  bunyan.ERROR(error);
+  // Retry retrieving request data a configurable amount of times, if fails respond with 502, if the retrieval times out after a configurable period, respond with 504
+  res.send(502);
+  Promise.reject(error);
+});
 
 module.exports = router
