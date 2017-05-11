a2eaddba4f41ec42e0acc660863e47b7b6c1dffc
Erez Sobel <sobelerez@gmail.com>
2017-05-11 14:58:34 +0200

    Log errors and add comments

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const bunyan = require('bunyan')
+
+// Initializing bunyan for logs
+const log = bunyan.createLogger({name: 'thumbnailer'})
 
 const thumbnailService = require('../thumbnail_service')
 
 router.route('/:urlBase64/:maxWidth/:maxHeight/:signatureBase64.:extension')
 .get(thumbnailService)
 .catch(error => {
-  bunyan.ERROR(error);
+  log.warn(error)
   // Retry retrieving request data a configurable amount of times, if fails respond with 502, if the retrieval times out after a configurable period, respond with 504
-  res.send(502);
-  Promise.reject(error);
-});
+  Promise.reject(error)
+})
 
 module.exports = router
--- thumbnail_service.js
+++ thumbnail_service.js
@@ -1,85 +1,99 @@
 // import gm from 'gm';
-const bunyan = require('bunyan');
-const { pipe } = require('lodash/fp');
-const validUrl = require('valid-url');
+const bunyan = require('bunyan')
+const { pipe } = require('lodash/fp')
+const validUrl = require('valid-url')
 
+// Initializing bunyan for logs
+const log = bunyan.createLogger({name: 'thumbnailer'})
+
+/**
+ * The main thumbnailer service function
+ */
 const thumbnailService = (req, res) => {
   // return validity(req, res);
 }
 
+/**
+ * Rescaling images
+ */
 // const rescale = req => {
 //   const decodedUrl = decode(req.params.urlBase64);
 
 // }
 
+/**
+ * Validating all the URI data using the helper validating functions
+ * Returns true or false
+ */
 const validity = (req, res) => {
-  const params = req.params;
-  const base64Data = verifyUrlBase64(params.urlBase64);
-  const maxWidthData = verifyMaxWidthHeight(params.maxWidth);
-  const maxHeightData = verifyMaxWidthHeight(params.maxHeight);
-  const signatureBase64Data = verifySignatureBase64(params.signatureBase64);
-  const extensionData = verifyExtension(params.extension);
+  const params = req.params
+  const base64Data = validateUrlBase64(params.urlBase64)
+  const maxWidthData = validateMaxWidthHeight(params.maxWidth)
+  const maxHeightData = validateMaxWidthHeight(params.maxHeight)
+  const signatureBase64Data = validateSignatureBase64(params.signatureBase64)
+  const extensionData = validateExtension(params.extension)
 
   if (base64Data && maxWidthData && maxHeightData && extensionData && signatureBase64Data) {
-    return true;
+    return true
   }
-  
+
   if (signatureBase64Data === false) {
-    res.status(403);
-    bunyan.ERROR('Signature is invalid')
-    return false;
+    res.status(403)
+    log.warn('Signature is invalid')
+    return false
   }
 
   if ((base64Data && maxWidthData && maxHeightData && extensionData) === false && signatureBase64Data === true) {
-    bunyan.ERROR('One ro some parameters that are not the signature have failed')    
-    res.status(400);
-    return false;    
+    log.warn('One ro some parameters that are not the signature have failed')
+    res.status(400)
+    return false
   }
-  return false;      
+  return false
 }
 
 /**
- * Composing the function to verify urlBase64
- * Return true or false
+ * Composing the function to validate urlBase64
+ * Returns true or false
  */
-const verifyUrlBase64 = x => pipe(
+const validateUrlBase64 = x => pipe(
   decode,
   checkUrlValidity
-);
+)
 
 /**
  * Check Url Validity
  * Returns true or false
  */
-const checkUrlValidity = x => validUrl.isUri(x) ? true : false;
+const checkUrlValidity = x => !!validUrl.isUri(x)
 
 /**
  * Decoding data from Base64 to regular string (URL string if it's valid)
- * Return the decoded string
+ * Returns the decoded string
  */
-const decode = x => Buffer.from(x, 'base64').toString('ascii');
+const decode = x => Buffer.from(x, 'base64').toString('ascii')
 
 /**
- * Verifying that the maxWidth and maxHeight are according to the rules
+ * Validating that the maxWidth and maxHeight are according to the rules
  * Returns true or false
  */
-const verifyMaxWidthHeight = x => {
-  return !isNaN(x) && +3 < x && x < +1024 ? true : false;
+const validateMaxWidthHeight = x => {
+  return !!(!isNaN(x) && +3 < x && x < +1024)
 }
 
-
-const verifySignatureBase64 = x => x;  
+/**
+ * Validate the signature
+ */
+const validateSignatureBase64 = x => x
 
 /**
  * Check that the property is one of the allowed extensions options
  * Returns true or false
  */
-const verifyExtension = x => /^jpeg|png|gif|ico|webm$/.test(x);
+const validateExtension = x => /^jpeg|png|gif|ico|webm$/.test(x)
 
-
-module.exports = thumbnailService;
-module.exports.checkUrlValidity = checkUrlValidity;
-module.exports.decode = decode;
-module.exports.verifyMaxWidthHeight = verifyMaxWidthHeight;
-module.exports.verifyExtension = verifyExtension;
-module.exports.validity = validity;
+module.exports = thumbnailService
+module.exports.checkUrlValidity = checkUrlValidity
+module.exports.decode = decode
+module.exports.validateMaxWidthHeight = validateMaxWidthHeight
+module.exports.validateExtension = validateExtension
+module.exports.validity = validity
